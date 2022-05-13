482a11117c7a157acc6b33023c641c5b8aaf1959
gazillion101 <igor.plotnikov@gmail.com>
2022-05-12 23:21:33 -0700

    improve backdrop
    Former-commit-id: ea668b49df5ebb8c5548408f051b4e90008d1c9f

--- web/portal/src/Backdrop.js
+++ web/portal/src/Backdrop.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { TweenLite, Circ } from "gsap";
 
 function Backdrop() {
   let width, height, canvas, ctx, points, target, animateHeader = true, cs, sn;
@@ -12,7 +11,6 @@ function Backdrop() {
   });
   // Main
 
-
   function initHeader() {
     width = window.innerWidth;
     height = window.innerHeight;
@@ -21,33 +19,27 @@ function Backdrop() {
 
     target = { x: centerx, y: centery };
 
-
     canvas = document.getElementById('anicanvas');
     canvas.width = width;
     canvas.height = height;
     ctx = canvas.getContext('2d');
 
     // create points
-    points = [];
+    points = []
     let angle = 3.14159 * 10 / 180
     cs = Math.cos(angle)
     sn = Math.sin(angle)
     let step = width / 18
-    for (let i = 1; i <= 6; i++) {
 
-      let rad = Math.abs(step * i)
-      let xorig =  sn * step * i
-      let yorig =  cs * step * i
-      let drad = Math.random() * rad / 5
-      let dx, dy
-      if (i > 0) {
-        dx = drad * sn
-        dy = drad * cs
-      } else {
-        dx = -drad * sn
-        dy = -drad * cs
+    for (let i = 2; i <= 10; i++) {
 
-      }
+      let rad = Math.abs(step * i*i/6)
+      let xorig =  sn * step * i*i/6
+      let yorig =  cs * step * i*i/6
+
+      let drad = Math.random() * rad * 4
+      let dx = drad * sn
+      let dy = drad * cs
 
       let p = {
         originX: xorig, originY: yorig, originR: rad,
@@ -55,8 +47,11 @@ function Backdrop() {
       };
 
       points.push(p);
-
     }
+    for(let i = 0; i < 50; i++) {
+      shakeField()
+    }
+
   }
   function draw(p) {
 
@@ -101,41 +96,53 @@ function Backdrop() {
     initAnimation()
   }
 
+  function  shakeField() {
+    function  mv(p) {
+      let dx, dy
+
+      let drad = Math.random() * p.originR * 4
+      dx = drad * sn
+      dy = drad * cs
+
+      drad = 0.005*drad + 0.995*p.drad
+
+
+      dx = 0.005*dx + 0.995*p.dx
+      dy = 0.005*dy + 0.995*p.dy
+
+      let alpha = Math.random() 
+      alpha = 0.1*alpha + 0.9*p.alpha
+
+      Object.assign(p,  { dx, dy, drad, alpha})
+    }
+    function avg(p0, p1) {
+      Object.assign(p1,  { dx: (p0.dx + p1.dx)/2, 
+        dy: (p0.dy + p1.dy)/2, drad: (p0.drad + p1.drad)/2, alpha: (p0.alpha + p1.alpha)/2})
+
+    }
+    for (let i in points) {
+      mv(points[i]);
+    }    
+     for(let i = 0; i < points.length - 1; i++) {
+       avg(points[i], points[i+1])
+     }
+  }
   // animation
   function initAnimation() {
     animate();
-    for (let i in points) {
-      shiftPoint(points[i]);
-    }
-  }
 
-  function animate() {
+  }
+  
+  function animate(timestamp) {
     if (animateHeader) {
       ctx.clearRect(0, 0, width, height);
       for (let i in points) {
-
         draw(points[i]);
       }
     }
+    shakeField()
     requestAnimationFrame(animate);
   }
-
-  function shiftPoint(p) {
-    let drad = Math.random() * p.originR / 5
-    let dx, dy
-
-    dx = drad * sn
-    dy = drad * cs
-
-    TweenLite.to(p, 2 + 2 * Math.random(), {dx, dy, drad, alpha: 0.5 + 0.6*Math.random() - 0.3,
-    ease: Circ.easeInOut,
-      onComplete: function () {
-        shiftPoint(p);
-      }
-    });
-  }
-
-
 
 
   return (
