29d837984d77959928371e76e0ef32a772b09e72
gazillion101 <igor.plotnikov@gmail.com>
2022-06-09 18:12:22 -0700

    update schema
    Former-commit-id: a19db1aefcc959669fdedd53f34cb3a320150704 [formerly 0d31d2251d182d601a2a41434f4b81216442f480]
    Former-commit-id: 1b963c66ac3431361820208e6906e7024a1a1bf9

--- setup/init.sql
+++ setup/init.sql
@@ -17,13 +17,13 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: dymium; Type: SCHEMA; Schema: -; Owner: igor
+-- Name: dymium; Type: SCHEMA; Schema: -; Owner: dymium
 --
 
 CREATE SCHEMA dymium;
 
 
-ALTER SCHEMA dymium OWNER TO igor;
+ALTER SCHEMA dymium OWNER TO dymium;
 
 --
 -- Name: global; Type: SCHEMA; Schema: -; Owner: dymium
@@ -77,6 +77,18 @@ CREATE TABLE global.customers (
 ALTER TABLE global.customers OWNER TO postgres;
 
 --
+-- Name: access; Type: TABLE; Schema: spoofcorp; Owner: dymium
+--
+
+CREATE TABLE spoofcorp.access (
+    datascope_id character varying(36) NOT NULL,
+    group_id character varying(36) NOT NULL
+);
+
+
+ALTER TABLE spoofcorp.access OWNER TO dymium;
+
+--
 -- Name: admincredentials; Type: TABLE; Schema: spoofcorp; Owner: postgres
 --
 
@@ -108,6 +120,18 @@ CREATE TABLE spoofcorp.connections (
 ALTER TABLE spoofcorp.connections OWNER TO postgres;
 
 --
+-- Name: datascopes; Type: TABLE; Schema: spoofcorp; Owner: dymium
+--
+
+CREATE TABLE spoofcorp.datascopes (
+    id character varying(36) DEFAULT public.uuid_generate_v4() NOT NULL,
+    name character varying(128) NOT NULL
+);
+
+
+ALTER TABLE spoofcorp.datascopes OWNER TO dymium;
+
+--
 -- Name: passwords; Type: TABLE; Schema: spoofcorp; Owner: postgres
 --
 
@@ -118,6 +142,41 @@ CREATE TABLE spoofcorp.passwords (
 
 
 ALTER TABLE spoofcorp.passwords OWNER TO postgres;
+
+--
+-- Name: refs; Type: TABLE; Schema: spoofcorp; Owner: dymium
+--
+
+CREATE TABLE spoofcorp.refs (
+    id character varying(36) DEFAULT public.uuid_generate_v4() NOT NULL,
+    schem character varying(128) NOT NULL,
+    tabl character varying(128) NOT NULL,
+    col character varying(128) NOT NULL
+);
+
+
+ALTER TABLE spoofcorp.refs OWNER TO dymium;
+
+--
+-- Name: tables; Type: TABLE; Schema: spoofcorp; Owner: dymium
+--
+
+CREATE TABLE spoofcorp.tables (
+    id character varying(36) DEFAULT public.uuid_generate_v4() NOT NULL,
+    datascope_id character varying(36) NOT NULL,
+    connection_id character varying(64) NOT NULL,
+    schem character varying(128) NOT NULL,
+    tabl character varying(128) NOT NULL,
+    action character varying(32) NOT NULL,
+    col character varying(128) NOT NULL,
+    typ character varying(32) NOT NULL,
+    "position" integer,
+    reference character varying(36),
+    semantics character varying(32) NOT NULL
+);
+
+
+ALTER TABLE spoofcorp.tables OWNER TO dymium;
 
 --
 -- Name: customers customers_pkey; Type: CONSTRAINT; Schema: global; Owner: postgres
@@ -152,11 +211,59 @@ ALTER TABLE ONLY spoofcorp.connections
 
 
 --
+-- Name: datascopes datascopes_name_key; Type: CONSTRAINT; Schema: spoofcorp; Owner: dymium
+--
+
+ALTER TABLE ONLY spoofcorp.datascopes
+    ADD CONSTRAINT datascopes_name_key UNIQUE (name);
+
+
+--
+-- Name: datascopes datascopes_pkey; Type: CONSTRAINT; Schema: spoofcorp; Owner: dymium
+--
+
+ALTER TABLE ONLY spoofcorp.datascopes
+    ADD CONSTRAINT datascopes_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: passwords passwords_pkey; Type: CONSTRAINT; Schema: spoofcorp; Owner: postgres
 --
 
 ALTER TABLE ONLY spoofcorp.passwords
     ADD CONSTRAINT passwords_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: refs refs_col_key; Type: CONSTRAINT; Schema: spoofcorp; Owner: dymium
+--
+
+ALTER TABLE ONLY spoofcorp.refs
+    ADD CONSTRAINT refs_col_key UNIQUE (col);
+
+
+--
+-- Name: refs refs_pkey; Type: CONSTRAINT; Schema: spoofcorp; Owner: dymium
+--
+
+ALTER TABLE ONLY spoofcorp.refs
+    ADD CONSTRAINT refs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tables tables_datascope_id_connection_id_schem_action_col_key; Type: CONSTRAINT; Schema: spoofcorp; Owner: dymium
+--
+
+ALTER TABLE ONLY spoofcorp.tables
+    ADD CONSTRAINT tables_datascope_id_connection_id_schem_action_col_key UNIQUE (datascope_id, connection_id, schem, action, col);
+
+
+--
+-- Name: tables tables_pkey; Type: CONSTRAINT; Schema: spoofcorp; Owner: dymium
+--
+
+ALTER TABLE ONLY spoofcorp.tables
+    ADD CONSTRAINT tables_pkey PRIMARY KEY (id);
 
 
 --
