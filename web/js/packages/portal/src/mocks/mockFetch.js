6139aacac3d99fa70a19e8dd0fbe5569dea7315f
gazillion101 <igor.plotnikov@gmail.com>
2022-07-01 13:55:20 -0700

    more tests
    Former-commit-id: 098b64d9a5851661deac0d024e413fd55bd99328 [formerly fe72d0ccb2a86b334e1d34f0912b94aac75fc0d7]
    Former-commit-id: 6517f4278617a719de0964f72417caee57c3cda6

--- web/js/packages/portal/src/mocks/mockFetch.js
+++ web/js/packages/portal/src/mocks/mockFetch.js
@@ -4,7 +4,6 @@ export default async function mockFetch(url, js) {
     console.log("in mock fetch: ")
     switch (url) {
         case "/api/getconnections": {
-            console.log(url)
             return  new Promise( (resolve, reject) => resolve(
                 {
                     ok: true,
@@ -14,7 +13,37 @@ export default async function mockFetch(url, js) {
               )
             )
         }
-
+        case "/api/getdatascopes" : {
+            return  new Promise( (resolve, reject) => resolve(
+                {
+                    ok: true,
+                    status: 200,
+                    json:  () => new Promise( (resolve, reject) => resolve(JSON.parse('{"status":"OK","errormessage":"","records":[{"name":"test1","id":"50fe96d1-b1a4-48df-87bd-6aced2ab1f5a"},{"name":"test2","id":"b3fddf89-5822-4227-9948-3ecb3c8c2e33"}]}') ))
+                }
+              )
+            )            
+        }
+        case "/api/getmappings" : {
+            return  new Promise( (resolve, reject) => resolve(
+                {
+                    ok: true,
+                    status: 200,
+                    json:  () => new Promise( (resolve, reject) => resolve(JSON.parse('{"status":"OK","errormessage":"","records":[{"id":"788b3c71-5371-41e8-b135-8037bcfcfd9f","dymiumgroup":"foo","directorygroup":"bar","comments":"Admin group fubar"},{"id":"27713f32-6772-4509-9476-41099b11cb18","dymiumgroup":"dada","directorygroup":"production","comments":"Comment changed via test"}]}') ))
+                }
+              )
+            )                     
+        }
+        
+        case "/api/getgroupsfordatascopes" : {
+            return  new Promise( (resolve, reject) => resolve(
+                {
+                    ok: true,
+                    status: 200,
+                    json:  () => new Promise( (resolve, reject) => resolve(JSON.parse('[{"id":"50fe96d1-b1a4-48df-87bd-6aced2ab1f5a","name":"test1","groupid":"788b3c71-5371-41e8-b135-8037bcfcfd9f","groupname":"foo"}]') ))
+                }
+              )
+            )                     
+        }        
         default: {
             throw new Error(`Unhandled request: ${url}`);
         }
---
+++ web/js/packages/portal/src/tests/GroupMapping.test.js
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { BrowserRouter } from "react-router-dom";
+import { act } from 'react-dom/test-utils';
+import { Provider } from 'react-redux'
+import store from '../Store/index'
+import { Http } from '../Api/Http'
+import  GroupMapping  from '../App/Groups'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@dymium/common/App.scss';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import mockFetch from "../mocks/mockFetch";
+
+
+beforeEach(() => {
+  global.fetch = jest.fn().mockImplementation(() => mockFetch)();
+  global.sessionStorage.setItem("Session", "mockJWT");
+})
+
+afterEach(() => {
+   jest.restoreAllMocks()
+});
+
+test('test-sql', async () => {
+    const component = await act( async () => renderer.create(
+        <Provider store={store}>
+            <BrowserRouter>
+                <GroupMapping />
+            </BrowserRouter>
+        </Provider >
+    ));
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
