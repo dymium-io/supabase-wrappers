bddc6d3104a0ed9e0b608414f283f1c960d9aaa3
gazillion101 <igor.plotnikov@gmail.com>
2022-07-13 18:39:03 -0700

    add missing files for admin

--- web/js/packages/admin/src/Utils/CustomHooks.js
+++ web/js/packages/admin/src/Utils/CustomHooks.js
@@ -1,7 +1,7 @@
 // borrowed from https://dev.to/bytebodger/constructors-in-functional-components-with-hooks-280m
 import { useRef } from 'react';
 
-export const useConstructor = (callBack) => {
+export const useInitialize = (callBack) => {
    const notfirst = useRef(false);
    if (notfirst.current)
       return;
