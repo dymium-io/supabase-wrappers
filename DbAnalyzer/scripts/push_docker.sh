c97ae6f0f48d7f7dcf00f4a176dc7d939940b8c8
Valentyn Kamyshenko (at Fortinbras) <valya@dymium.io>
2022-07-26 20:38:47 -0700

    add additional argument to push_docker scripts to specify account

--- DbAnalyzer/scripts/push_docker.sh
+++ DbAnalyzer/scripts/push_docker.sh
@@ -2,18 +2,40 @@
 
 set -e
 
-ARN="411064808315"
 REPO="db-analyzer"
 REGION="us-west-2"
 
+case "$1" in
+    "dymium")
+	ARN="411064808315"
+	PROFILE="dymium"
+	shift
+	;;
+    "staging")
+	ARN="626593984035"
+	PROFILE="dymium_staging"
+	shift
+	;;
+    "dev")
+	ARN="564835066653"
+	PROFILE="dymium_dev"
+	shift
+	;;
+    *)
+	echo "option '$1' is not supported"
+	echo "Usage: $0 [dymium|staging|dev] [latest|prod]"
+	exit 255
+	;;
+esac
+
 tag="${1:-latest}"
 [ $tag = "latest" -o $tag = "prod" ] || {
-    echo "Usage: ./push_docker.sh [tag]"
+    echo "Usage: $0 [staging|dev] [tag]"
     echo "  where tag must be equal to 'latest' or 'prod'"
     exit 1
 }
 
 docker tag ${REPO}:latest ${ARN}.dkr.ecr.${REGION}.amazonaws.com/${REPO}:${tag}
 
-aws ecr get-login-password --profile dymium --region $REGION | docker login --username AWS --password-stdin ${ARN}.dkr.ecr.${REGION}.amazonaws.com
+aws ecr get-login-password --profile $PROFILE --region $REGION | docker login --username AWS --password-stdin ${ARN}.dkr.ecr.${REGION}.amazonaws.com
 docker push ${ARN}.dkr.ecr.${REGION}.amazonaws.com/${REPO}:${tag}
--- DbGuardian/scripts/restart.sh
+++ DbGuardian/scripts/restart.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-PROFILE=dymium
 REGION=us-west-2
 # ARN="411064808315"
 # REPO="db-sync"
 
+PROFILE="dymium"
+
+if [ "$1" = "staging" ]
+then
+    PROFILE="dymium_staging"
+    shift
+elif [ "$1" = "dev" ]
+then
+    PROFILE="dymium_dev"
+    shift
+fi
 
 case "$1" in
   "")
