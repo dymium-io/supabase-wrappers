ad13868446995cb1776ece4430c75cf4aacbc157
Valentyn Kamyshenko (at Fortinbras) <valya@dymium.io>
2022-09-12 16:37:31 -0700

    remove spoofcorp. from the table [users] definition

--- DbConf/customer/users.sql
+++ DbConf/customer/users.sql
@@ -2,7 +2,7 @@
 -- name: "customer/users",
 -- description: "Database users";
 
-CREATE TABLE spoofcorp.users (
+CREATE TABLE users (
     id varchar(36) PRIMARY KEY DEFAULT public.uuid_generate_v4(),
     username varchar(128) NOT NULL UNIQUE,
     password varchar(128) NOT NULL,
