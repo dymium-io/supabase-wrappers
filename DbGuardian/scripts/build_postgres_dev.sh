3557f2b982d78e7e105b9566b7363abab6b86758
Valentyn Kamyshenko (at Zeus) <kamysh@kamysh.org>
2022-09-22 23:36:16 -0700

    [tds_fdw] original data

--- DbGuardian/scripts/build_postgres_dev.sh
+++ DbGuardian/scripts/build_postgres_dev.sh
@@ -3,5 +3,9 @@
 cat <<\EOF | docker build -t postgres-dev -
 FROM alpine:3.16
 
-RUN apk add build-base postgresql14-dev mysql-client mariadb-connector-c-dev
+RUN apk add \
+    build-base \
+    postgresql14-dev \
+    mysql-client mariadb-connector-c-dev \
+    freetds-dev
 EOF
