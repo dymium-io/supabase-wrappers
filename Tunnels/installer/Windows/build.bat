fee7ad539601429a0c9610b49600d13212f37abe
Igor Plotnikov <40214964+gazillion101@users.noreply.github.com>
2022-10-04 10:23:50 -0700

    Create build.bat

---
+++ Tunnels/installer/Windows/build.bat
@@ -0,0 +1,7 @@
+
+
+cd ..\..\go\client
+go build -a -tags netgo -ldflags "-X ^"main.MajorVersion=0^" -X ^"main.MinorVersion=1^" -X ^"main.ProtocolVersion=4^" -extldflags ^"-static^"" -o dymium.exe
+copy dymium.exe ..\..\installer\Windows
+cd   ..\..\installer\Windows
+makensis tunnel.nsi
