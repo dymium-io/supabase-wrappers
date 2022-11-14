c227268905aa542408547b2314d80e788ea3e7fe
Igor Plotnikov <igor@dymium.io>
2022-11-14 15:31:53 -0800

    Update buildwin.bat

--- Tunnels/go/GUI/buildwin.bat
+++ Tunnels/go/GUI/buildwin.bat
@@ -1,4 +1,4 @@
-go build -a -tags netgo -ldflags "-X 'main.MajorVersion=0' -X 'main.MinorVersion=2' -X 'main.ProtocolVersion=4'-w -extldflags '-static'"
+go build -a -tags netgo -ldflags "-X 'main.MajorVersion=0' -X 'main.MinorVersion=5' -X 'main.ProtocolVersion=5'-w -extldflags '-static'"
 
 fyne package -os windows -icon logo.png
 
