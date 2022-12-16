1db8b1ab881260d5c57c56ab4a8a35c8dabc3829
Valentyn Kamyshenko (at Fortinbras) <kamysh@kamysh.org>
2022-12-15 23:54:17 -0800

    [suborgs-support] push-docker scripts reworked II (WIP)

--- DbGuardian/scripts/push_docker.sh
+++ DbGuardian/scripts/push_docker.sh
@@ -6,7 +6,7 @@ REPO="data-guardian"
 source "../../libs/shell/aws-include.sh"
 aws_params "$@"
 
-echo docker tag ${REPO}:latest ${ARN}.dkr.ecr.${REGION}.amazonaws.com/${REPO}:${tag}
+docker tag ${REPO}:latest ${ARN}.dkr.ecr.${REGION}.amazonaws.com/${REPO}:${tag}
 
 set -x
 aws ecr get-login-password \
@@ -14,4 +14,4 @@ aws ecr get-login-password \
     --region $REGION |
     docker login --username AWS \
 	   --password-stdin ${ARN}.dkr.ecr.${REGION}.amazonaws.com
-echo docker push ${ARN}.dkr.ecr.${REGION}.amazonaws.com/${REPO}:${tag}
+docker push ${ARN}.dkr.ecr.${REGION}.amazonaws.com/${REPO}:${tag}
