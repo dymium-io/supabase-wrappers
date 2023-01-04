bca3cd4c304e9ed1f0f402e31479f4d42e12b29d
Valentyn Kamyshenko (at Fortinbras) <kamysh@kamysh.org>
2023-01-03 16:57:36 -0800

    restart script for the meshserver

--- Tunnels/go/meshscripts/server/restart.sh
+++ Tunnels/go/meshscripts/server/restart.sh
@@ -19,7 +19,7 @@ aws_params -exe "$0 -c <customer> " "$@"
 set -x
 aws ecs update-service \
     --cluster "dymium" \
-    --service "${c}-tunnel-srv" \
+    --service "${c}-meshserver-srv" \
     --force-new-deployment \
     --profile ${PROFILE} \
     --region ${REGION}
