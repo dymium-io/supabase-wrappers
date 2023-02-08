902b1a31ad857c8defe9b37453f9ed7bcf8cadee
gazillion101 <igor.plotnikov@gmail.com>
2023-02-07 18:33:34 -0800

    Admin help. fix for creditcard regexp

--- web/js/packages/admin/src/App/Sidebar.tsx
+++ web/js/packages/admin/src/App/Sidebar.tsx
@@ -16,15 +16,19 @@ export default function Sidebar() {
   return (
     <div className="sidenav h-100" id="sidebar">
       <Link className='hover-sidebar'
-        to='/app/dashboard' > <div className='darkblue'> <i className="fas fa-tachometer-alt mr-1"></i>Dashboard</div></Link >
+        to='/app/dashboard' > <div className='darkblue'> <i className="fas  fa-fw fa-tachometer-alt mr-1"></i>Dashboard</div></Link >
 
       <Link className='hover-sidebar'
-        to='/app/customers' > <div className='darkblue'> <i className="fa fa-users mr-1"></i>Customers</div></Link >
+        to='/app/customers' > <div className='darkblue'> <i className="fa  fa-fw fa-users mr-1"></i>Customers</div></Link >
+
+      <Link className='hover-sidebar'
+        to='/app/help' > <div className='darkblue'> <i className="fa  fa-fw fa-question-circle mr-1"></i>HowTos</div></Link >
+
 
       <a className='hover-sidebar'
         href="/app/logout"
       >
-         <div className='darkblue'> <i className="fa fa-sign-out" aria-hidden="true"></i>Logout </div></a >
+        <div className='darkblue'> <i className="fa  fa-fw fa-sign-out" aria-hidden="true"></i>Logout </div></a >
 
     </div>
 
