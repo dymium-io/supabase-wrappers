e2ae648f943e9c6461d43b42391f749da7a15681
gazillion101 <igor.plotnikov@gmail.com>
2023-05-04 18:35:44 -0700

    [fix-search-in-table] fix broken toolkit

--- web/go/scripts/build_docker.sh
+++ web/go/scripts/build_docker.sh
@@ -57,29 +57,19 @@ cd ../../../Tunnels/go/client
 
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
-           go build -a -tags netgo -ldflags '-X 'main.MajorVersion=0' -X 'main.MinorVersion=6' -X 'main.ProtocolVersion=6' -w -extldflags "-static"' -o tunnel
-chmod a+x tunnel
+           go build -a -tags netgo -ldflags '-X 'main.MajorVersion=0' -X 'main.MinorVersion=6' -X 'main.ProtocolVersion=6' -w -extldflags "-static"' -o dymium
+chmod a+x dymium
 mkdir -p ../../../web/go/assets/customer/update/linux/amd64/
 cp tunnel ../../../web/go/assets/customer/update/linux/amd64/
-tar -zcvf tunnel.tar.gz tunnel
+tar -zcvf tunnel.tar.gz dymium
 cp tunnel.tar.gz ../../../web/go/assets/customer/update/
 mv tunnel.tar.gz ../../../web/js/packages/portal/public
 
-
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 \
-           go build -a -tags netgo -ldflags '-X 'main.MajorVersion=0' -X 'main.MinorVersion=6' -X 'main.ProtocolVersion=6' -extldflags "-static"' -o tunnel.exe
-mkdir -p ../../../web/go/assets/customer/update/windows/amd64/
-cp tunnel.exe ../../../web/go/assets/customer/update/windows/amd64/tunnel
 aws s3  --profile dymium --region us-west-2 cp s3://dymium-installers/windows/DymiumInstaller.exe /tmp
 cp /tmp/DymiumInstaller.exe ../../../web/go/assets/customer/
 mv /tmp/DymiumInstaller.exe ../../../web/js/packages/portal/public
 
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
-           go build -a -tags netgo -ldflags '-X 'main.MajorVersion=0' -X 'main.MinorVersion=6' -X 'main.ProtocolVersion=6' -w -extldflags "-static"' -o tunnel
-chmod a+x tunnel
-mkdir -p ../../../web/go/assets/customer/update/darwin/amd64/
-cp tunnel ../../../web/go/assets/customer/update/darwin/amd64/
 aws s3  --profile dymium --region us-west-2 cp s3://dymium-installers/macos/DymiumInstaller.pkg /tmp
 
 cp /tmp/DymiumInstaller.pkg ../../../web/go/assets/customer/
