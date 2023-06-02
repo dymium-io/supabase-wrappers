0bebbf5dc014ce32d4f351e63b67f7fa16f17cef
mickk939 <129130351+mickk939@users.noreply.github.com>
2023-06-01 17:12:21 -0700

    [lc_passwdremove] Obfuscate passwors in log statement. In Dev log simple query shows that we have few types of statements that contains passwords: "create mapping" and "create/alter user". Added obfuscation in logcollector and test cases for found statement types.

---
+++ logcollector/go/logprocessor_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestObfuscatePasswords(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Obfuscate passwords in 'CREATE USER MAPPING' statement",
+			args: args{
+				msg: "statement: CREATE USER MAPPING FOR _3381fa486e704495c5e6e9c38aa2e5a6_ SERVER maria_server OPTIONS (username 'root', password '1234')",
+			},
+			want: "statement: CREATE USER MAPPING FOR _3381fa486e704495c5e6e9c38aa2e5a6_ SERVER maria_server OPTIONS (username 'root', password '********')",
+		},
+		{
+			name: "Obfuscate passwords in 'CREATE USER SUPERUSER' statement",
+			args: args{
+				msg: "statement: CREATE USER username SUPERUSER PASSWORD 'xsdTY7MRpv*LoGmBXMez'",
+			},
+			want: "statement: CREATE USER username SUPERUSER PASSWORD '********'",
+		},
+		{
+			name: "Obfuscate passwords in 'CREATE USER' statement",
+			args: args{
+				msg: "statement: CREATE USER username PASSWORD 'xsdTY7MRpv*LoGmBXMez'",
+			},
+			want: "statement: CREATE USER username PASSWORD '********'",
+		},
+		{
+			name: "Obfuscate passwords in 'ALTER USER' statement, case 1",
+			args: args{
+				msg: "statement: ALTER USER edison WITH ENCRYPTED PASSWORD '=UOBAgxY9d'",
+			},
+			want: "statement: ALTER USER edison WITH ENCRYPTED PASSWORD '********'",
+		},
+		{
+			name: "Obfuscate passwords in 'ALTER USER' statement, case 2",
+			args: args{
+				msg: "statement: ALTER USER edison WITH PASSWORD '=UOBAgxY9d'",
+			},
+			want: "statement: ALTER USER edison WITH PASSWORD '********'",
+		},
+		{
+			name: "Do not obfuscate passwords in other statements",
+			args: args{
+				msg: "statement: CREATE FOREIGN TABLE Person.Password (\n  " +
+					"BusinessEntityID integer OPTIONS( redact '17' ) NOT NULL,\n " +
+					" PasswordHash varchar(128) OPTIONS( redact '10' ) NOT NULL,\n  " +
+					"PasswordSalt varchar(10) OPTIONS( redact '10' ) NOT NULL,\n  " +
+					"ModifiedDate timestamp (3) without time zone OPTIONS( redact '65' ) " +
+					"NOT NULL\n)\n  SERVER sql_server_adventureworks_server " +
+					"OPTIONS(schema_name 'Person', table_name 'Password')",
+			},
+			want: "statement: CREATE FOREIGN TABLE Person.Password (\n  " +
+				"BusinessEntityID integer OPTIONS( redact '17' ) NOT NULL,\n " +
+				" PasswordHash varchar(128) OPTIONS( redact '10' ) NOT NULL,\n  " +
+				"PasswordSalt varchar(10) OPTIONS( redact '10' ) NOT NULL,\n  " +
+				"ModifiedDate timestamp (3) without time zone OPTIONS( redact '65' ) " +
+				"NOT NULL\n)\n  SERVER sql_server_adventureworks_server " +
+				"OPTIONS(schema_name 'Person', table_name 'Password')",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ObfuscatePasswords(tt.args.msg); got != tt.want {
+				t.Errorf("ObfuscatePasswords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
