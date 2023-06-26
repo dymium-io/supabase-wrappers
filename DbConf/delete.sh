6be39647ee36fdb19ac2cbb5c7dc35ede6ad478e
Valentyn Kamyshenko (at Zeus) <kamysh@kamysh.org>
2023-06-25 18:19:17 -0700

    [main] DbConf: check that arg to delete.sh is specified

--- DbConf/delete.sh
+++ DbConf/delete.sh
@@ -7,6 +7,11 @@ DATABASE_PORT=${DATABASE_PORT:-5432}
 DATABASE_DB=${DATABASE_DB:-dymium}
 DATABASE_USER=${DATABASE_USER:-dymium}
 
+[ -z "$1" ] && {
+    echo "Usage: $0 <customer>"
+    exit 255
+}
+
 
 ../bin/mallard drop -s $1  \
 		--host ${DATABASE_HOST} --port ${DATABASE_PORT} \
