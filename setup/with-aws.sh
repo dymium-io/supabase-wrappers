e871df49d1b8851bc18ca0af266ca7d74344c245
Valentyn Kamyshenko (at Hamlet) <kamysh@kamysh.org>
2023-10-30 15:53:29 -0700

    utility script to work with AWS credentials

---
+++ setup/with-aws.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+    '--profile' | '-profile')
+	prf="$2"
+	shift 2
+	;;
+    *)
+	prf='dymium-dev'
+	;;
+esac
+
+eval $(sed -n -e '/^\['"$prf"'\]/,/\[/!d' -e '/^\[/d; /^ *$/d; s/^\([^ ]*\) *= *\(.*\)$/\1='"'"'\2'"'"';/;p' $HOME/.aws/credentials)
+export AWS_ACCESS_KEY=${aws_access_key_id}
+export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
+export AWS_SESSION_TOKEN=${aws_session_token}
+exec "$@"
