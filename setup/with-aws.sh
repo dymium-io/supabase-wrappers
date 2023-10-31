e94fb711fb1ed1e21255e4d581408c7ea8021bc1
Valentyn Kamyshenko (at Hamlet) <kamysh@kamysh.org>
2023-10-30 21:29:17 -0700

    bug fix in setup/with-aws.sh script

--- setup/with-aws.sh
+++ setup/with-aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 case "$1" in
     '--profile' | '-profile')
@@ -13,7 +13,7 @@ case "$1" in
 esac
 
 eval $(sed -n -e '/^\['"$prf"'\]/,/\[/!d' -e '/^\[/d; /^ *$/d; s/^\([^ ]*\) *= *\(.*\)$/\1='"'"'\2'"'"';/;p' $HOME/.aws/credentials)
-export AWS_ACCESS_KEY="${aws_access_key_id}"
+export AWS_ACCESS_KEY_ID="${aws_access_key_id}"
 export AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}"
 export AWS_SESSION_TOKEN="${aws_session_token}"
 "$@"
