3ae5efbc6000c602e5c235791ddbf7b18f829634
Valentyn Kamyshenko (at Zeus) <kamysh@kamysh.org>
2023-11-11 16:30:19 -0800

    [main] Remove mallard subtree to be re-added later as external subtree repo

--- Utils/mallard/scripts/deb-package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cabal build
-mkdir -p /tmp/installdir/usr/local/bin
-cp dist/build/mallard/mallard /tmp/installdir/usr/local/bin/mallard
-fpm -s dir -t deb -n mallard -v 0.6.3.0 -C /tmp/installdir \
-        -d libpq5 \
-        usr/local/bin/mallard
