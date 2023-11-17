6b2880b4123215043408222508b7917735c3e4b7
Valentyn Kamyshenko (at Zeus) <kamysh@kamysh.org>
2023-11-16 17:05:10 -0800

    [main] configuration flags for mallard

--- default.nix
+++ default.nix
@@ -1,56 +1,30 @@
-{ mkDerivation, base, byteable, bytestring, cryptohash, exceptions, fgl
-, file-embed, foldl, hashable, hasql, hasql-pool, hasql-th, hasql-transaction
-, Interpolation, lens, lib, megaparsec, mtl, optparse-applicative, optparse-text
-, path, path-io, text, time, unordered-containers, vector }:
+{ mkDerivation, base, byteable, bytestring, cryptohash, exceptions
+, fgl, file-embed, foldl, hashable, hasql, hasql-pool, hasql-th
+, hasql-transaction, Interpolation, lens, lib, megaparsec, mtl
+, optparse-applicative, optparse-text, path, path-io, text, time
+, unordered-containers, vector
+}:
 mkDerivation {
   pname = "mallard";
   version = "0.6.3.0";
+  buildTarget = exe:mallard;
   src = ./.;
-  isLibrary = false;
+  doCheck = false;
+  doHaddock = false;
+  enableLibraryProfiling = false;
+  enableSharedLibraries = false;
+  enableStaticLibraries = false;
+  isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base
-    byteable
-    bytestring
-    cryptohash
-    exceptions
-    fgl
-    file-embed
-    foldl
-    hashable
-    hasql
-    hasql-pool
-    hasql-th
-    hasql-transaction
-    Interpolation
-    lens
-    megaparsec
-    mtl
-    path
-    path-io
-    text
-    unordered-containers
-    vector
+    base byteable bytestring cryptohash exceptions fgl file-embed foldl
+    hashable hasql hasql-pool hasql-th hasql-transaction Interpolation
+    lens megaparsec mtl path path-io text unordered-containers vector
   ];
   executableHaskellDepends = [
-    base
-    exceptions
-    fgl
-    hasql
-    hasql-pool
-    lens
-    mtl
-    optparse-applicative
-    optparse-text
-    path
-    path-io
-    text
-    time
-    unordered-containers
+    base exceptions fgl hasql hasql-pool lens mtl optparse-applicative
+    optparse-text path path-io text time unordered-containers
   ];
-  configureFlags = [ "--disable-shared" ];
-  doHaddock = false;
-  doCheck = false;
   homepage = "https://github.com/AndrewRademacher/mallard";
   description = "Database migration and testing as a library";
   license = lib.licenses.mit;
