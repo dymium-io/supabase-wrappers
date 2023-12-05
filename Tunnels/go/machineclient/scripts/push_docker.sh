ad70b6a65928948a986b565f23a81f7d9c4a0071
gazillion101 <igor.plotnikov@gmail.com>
2023-12-04 18:41:19 -0800

    [#638] #638

--- Tunnels/go/machineclient/scripts/push_docker.sh
+++ Tunnels/go/machineclient/scripts/push_docker.sh
@@ -11,9 +11,9 @@ then
   tag="$1"
 fi
 
-docker tag dymiumconnector:latest public.ecr.aws/a9d3u0m7/dymiumcmachinetunnel:${tag}
-docker tag dymiumconnector:latest public.ecr.aws/a9d3u0m7/dymiumcmachinetunnel:latest 
+docker tag dymiumconnector:latest public.ecr.aws/a9d3u0m7/dymiummachinetunnel:${tag}
+docker tag dymiumconnector:latest public.ecr.aws/a9d3u0m7/dymiummachinetunnel:latest 
 aws ecr-public get-login-password --region us-east-1 --profile dymium | docker login --username AWS --password-stdin public.ecr.aws/a9d3u0m7
 
-docker push public.ecr.aws/a9d3u0m7/dymiumcmachinetunnel:latest
-docker tag  public.ecr.aws/a9d3u0m7/dymiumcmachinetunnel:latest public.ecr.aws/a9d3u0m7/dymiumcmachinetunnel:${tag}
+docker push public.ecr.aws/a9d3u0m7/dymiummachinetunnel:latest
+docker tag  public.ecr.aws/a9d3u0m7/dymiummachinetunnel:latest public.ecr.aws/a9d3u0m7/dymiummachinetunnel:${tag}
